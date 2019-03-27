8e465ce08b2a063c5fa3bf5e3b3af10075fc267f
Joshua-Gordon <joshua.sam.gordon@gmail.com>
2019-03-27 16:33:09 -0400

    Create Types.hs

---
+++ CollabProver/Types.hs
@@ -0,0 +1,34 @@
+module Types where
+
+{-
+ - PARSING LAND
+ -}
+data Formula = Var String
+              |Function String [Formula]
+              |And [Formula]
+              |Or [Formula]
+              |Not Formula
+              |Implies Formula Formula
+              |Quantified Quantifier Formula
+              deriving (Eq,Show)
+
+data Quantifier = Forall String | Exists String deriving (Eq,Show)
+
+{- DMZ -}
+type ID = Int
+
+{-
+- PROVER LAND
+-}
+--Represents a logical claim about an object in the Herbrand universe
+--The Bool indicates whether or not the predicate is negated, the ID is the ID value, and the [Term] is the args
+data Predicate = P Bool ID [Term]
+
+--Symbols represent functions and constants.
+--Constants are nullary functions.
+data Term = V ID
+           |Symbol ID [Term]
+            deriving (Eq,Show)
+
+--An `or` of a list of predicates
+type Clause = [Predicate]
